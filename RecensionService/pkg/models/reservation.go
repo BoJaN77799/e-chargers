c3b364d789c1e06df024bad746c5c9fd273e23d9
Bojan <bojanbaskalo@hotmail.rs>
2022-08-20 00:02:53 +0200

    recension service initialized

---
+++ RecensionService/pkg/models/reservation.go
@@ -0,0 +1,29 @@
+package models
+
+import (
+	"github.com/jinzhu/gorm"
+)
+
+type Recension struct {
+	gorm.Model
+	Username  string `json:"username" gorm:"not-null"`
+	ChargerId uint   `json:"charger_id" gorm:"not-null"`
+	Date      uint64 `json:"date"  gorm:"not-null"`
+	Content   string `json:"content" gorm:"not-null"`
+	Rate      uint   `json:"rate" gorm:"not-null"`
+}
+
+func (recension *Recension) ToDTO() RecensionDTO {
+	return RecensionDTO{
+		Username:  recension.Username,
+		ChargerId: recension.ChargerId,
+		Date:      recension.Date,
+		Content:   recension.Content,
+		Rate:      recension.Rate,
+	}
+}
+
+type CancelRecension struct {
+	Username  string `json:"username"`
+	ChargerId uint   `json:"charger_id"`
+}
