e63687fbaf76d0e2d76aae96538cc0e15b4ad651
Bojan <bojanbaskalo@hotmail.rs>
2022-08-19 22:32:52 +0200

    report_service init

---
+++ reports_service/src/main.rs
@@ -0,0 +1,49 @@
+#![feature(proc_macro_hygiene, decl_macro)]
+
+extern crate rocket;
+
+extern crate reqwest;
+
+use reqwest::Error;
+
+use rocket::{get, routes};
+use rocket_contrib::json::Json;
+
+use std::collections::HashMap;
+
+pub mod charger_service;
+pub mod types;
+
+use charger_service::*;
+use types::*;
+
+#[get("/chargers/<date_from>/<date_to>")]
+fn get_reservations(date_from: u64, date_to: u64) -> Result<Json<Report>, Error> {
+    let reservations: Vec<ReservationDTO> = get_all_reservations_in_period(date_from, date_to)
+        .ok()
+        .unwrap();
+
+    let map_chargers: HashMap<u32, ChargerDTO> = get_all_chargers(&reservations);
+
+    let map_report_items: HashMap<u32, ReportItem> =
+        create_report_items(&reservations, map_chargers);
+
+    let report: Report = Report {
+        date_from: date_from,
+        date_to: date_to,
+        chargers: map_report_items,
+    };
+
+    Ok(Json(report))
+}
+
+#[get("/")]
+fn index() -> &'static str {
+    "Hello, world."
+}
+
+fn main() {
+    rocket::ignite()
+        .mount("/api/reports", routes![index, get_reservations])
+        .launch();
+}
