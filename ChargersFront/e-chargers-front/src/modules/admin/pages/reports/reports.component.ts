59cad2dd67f26d4de3ba38417ec075608982572f
Bojan <bojanbaskalo@hotmail.rs>
2022-08-21 10:13:09 +0200

    energy consumption and money earned report added

--- ChargersFront/e-chargers-front/src/modules/admin/pages/reports/reports.component.ts
+++ ChargersFront/e-chargers-front/src/modules/admin/pages/reports/reports.component.ts
@@ -3,7 +3,7 @@ import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import * as moment from 'moment';
 import { ReservationDateValidator } from 'src/modules/reservation/validators/ReservationDateValidator';
 import { SnackBarService } from 'src/modules/shared/service/snack-bar.service';
-import { Report } from '../../model/reportsDTO';
+import { Report, ReportItem } from '../../model/reportsDTO';
 import { ReportsService } from '../../service/reportsService';
 
 @Component({
@@ -17,6 +17,8 @@ export class ReportsComponent implements OnInit {
 
   chargersReport!: Report;
 
+  reportItems: ReportItem[]
+
   constructor(
     private fb: FormBuilder,
     private snackBarService: SnackBarService,
@@ -28,6 +30,7 @@ export class ReportsComponent implements OnInit {
       date_from: [startDateTime, [Validators.required]],
       date_to: [endDateTime, [Validators.required]],
     });
+    this.reportItems = []
   }
 
   ngOnInit(): void {
@@ -35,16 +38,20 @@ export class ReportsComponent implements OnInit {
 
   submit() {
     let startDateTime = moment(this.reservationForm.get("date_from")?.value)
-    let endDateTime = moment(this.reservationForm.get("date_from")?.value)
+    let endDateTime = moment(this.reservationForm.get("date_to")?.value)
 
     if (startDateTime > endDateTime) {
       this.snackBarService.openSnackBar("Invalid dates - start is after or equal end")
       return
     }
 
-    this.reportsService.getReport(startDateTime.unix(), endDateTime.unix()).subscribe(
+    this.reportsService.getReport(startDateTime.valueOf(), endDateTime.valueOf()).subscribe(
       (response) => {
         this.chargersReport = response.body as Report;
+        const iterrableMap = new Map(Object.entries(this.chargersReport.chargers));
+        for (let [key, value] of iterrableMap) {
+          this.reportItems.push(value)
+        }
       },
       (err) => {
         this.snackBarService.openSnackBar("There is no report for this period.")
